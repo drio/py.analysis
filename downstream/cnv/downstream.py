94b52d4e327ad99ed69910d4666710e94021e0a0
David Rio Deiros <driodeiros@gmail.com>
2013-07-22 16:50:05 -0500

    Perform downstream analysis for cnv data.

---
+++ downstream/cnv/downstream.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+import drdplots
+import numpy as np
+from pandas import *
+import matplotlib.pyplot as plt
+from pylab import *
+from collections import defaultdict
+
+def misassembly(v, _max):
+  # v: value call in all samples ; max: maximun number of # of samples with the same call
+  return len(v[v==1]) >= _max or len(v[v==-1]) >= _max
+
+def count_num_of_events(df):
+  samples = defaultdict(lambda: {'i':0, 'd':0})
+
+  for name, bins in df.iterrows():
+    v = bins.values # all the RD values in all the bins for sample name
+    samples[name]['i'] = len(v[v==1])
+    samples[name]['d'] = len(v[v==-1])
+
+  return DataFrame(samples).transpose()
+
+def call_it(df, th=1):
+  def logic(v):
+    if v > th:
+      return 1
+    elif v < -th:
+      return -1
+    else:
+      return 0
+  return df.applymap(logic)
+
+def filter_bins_where_all_samples_have_same_call(df, _max):
+  n = 0
+  for coor in df.columns:
+    if misassembly(df[coor], _max):
+      del df[coor]
+  return df
+
+def normalize(df, rd_ref):
+  samples = defaultdict(lambda: [])
+
+  for name, bins in df.iterrows():
+    avg = np.average(bins.values)
+    new_values = bins.values*(rd_ref/avg)
+    new_values = new_values.astype(int)
+    samples[name] = new_values
+
+  return DataFrame(samples).transpose()
+
+def adjust_resolution(df, n_win=10):
+  samples = defaultdict(lambda: [])
+
+  for name, bins in df.iterrows():
+    b_values = []
+    indices, _tmp_i  = [], []
+    for i, v in enumerate(bins.values):
+      b_values.append(v)
+      _tmp_i.append(i)
+      if i % n_win == 0:
+        samples[name].append(np.average(b_values))
+        b_values = []
+        indices.append(_tmp_i[0])
+        _tmp_i   = []
+    # Last bin
+    if len(b_values) > 0:
+      samples[name].append(np.average(b_values))
+      indices.append(_tmp_i[0])
+
+  return DataFrame(samples, index=indices).transpose()
+
+def plot_hm(data, output_name):
+  fig = plt.figure(figsize=(21,10))
+  ax = fig.add_subplot(111)
+  ax.set_xlabel('chrm')
+  ax.set_ylabel('sample')
+  #ax.set_axis_off()
+  fig.add_axes(ax)
+  cax = ax.imshow(data, aspect = 'normal', interpolation='nearest', cmap=cm.Blues)
+  #cax = ax.imshow(data, aspect = 'normal', cmap=cm.Blues)
+  cbar = fig.colorbar(cax, orientation='horizontal')
+  #ax.pcolor(data)
+  savefig(output_name)
+
+def bar_plot(df, output_name):
+  fig = plt.figure(figsize=(18,4))
+  ax = fig.add_subplot(111)
+  ai, ad = df['i'], df['d']
+  sample_ids = df['i'].index # todo
+  ax.bar(range(len(ai)), ai)
+  print sample_ids
+  ax.set_xticklabels(sample_ids)
+  plt.setp(ax.get_xticklabels(), rotation='vertical')
+  savefig(output_name)
+
+def load_data(fn):
+  # index: coordinates; columns: samples
+  df = read_table(fn, sep=" ")
+  coordinates = df['start']
+  del df['chrm']; del df['start']
+  h = {}
+  for name, values in df.transpose().iterrows():
+    h[name] = []
+    for i, v in enumerate(values):
+      h[name].append(v)
+
+  return DataFrame(h, index=coordinates).transpose()
+
+def run(fn):
+  df = load_data(fn)
+  df = normalize(df, 300)
+  df = adjust_resolution(df, 10)
+  df = np.log2(df/300)
+  df = call_it(df, th=2)
+  print "# bins after adjusting resolution: %d " % len(df.columns)
+  df = filter_bins_where_all_samples_have_same_call(df, 50)
+  print "# bins after dropping bins where all samples have same call: %d" % len(df.columns)
+  plot_hm(df, 'headmap.png')
+  df.to_csv("calls.txt", sep="\t")
+
+  df = count_num_of_events(df)
+  df = df.drop('35084')
+  drdplots.barplot(df['i'], df['i'].index, 'insertions', 'ins.png', x_inches=20, y_inches=5, rota=90)
+  drdplots.barplot(df['d'], df['d'].index, 'deletions', 'del.png', x_inches=20, y_inches=5, rota=90)
+  df.to_csv("counts.txt", sep="\t")
+
+run("joined.35087.1k.300.depth.csv")
+#run("small.join.csv")
