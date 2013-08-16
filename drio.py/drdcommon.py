18e22a0e254c492c9b6757db1fb52a2a26749b1f
David Rio Deiros <driodeiros@gmail.com>
2013-08-16 14:56:28 -0400

    Tell me in O(1) if a particular locus in a genome is being hit. It is up to you to decide what is a hit.

--- drio.py/drdcommon.py
+++ drio.py/drdcommon.py
@@ -2,6 +2,28 @@ import sys, gzip, bz2
 import os, errno
 import glob
 from time import gmtime, strftime
+import re
+
+_human_genome_sizes = {
+  '1': 249250621, '2': 243199373, '3': 198022430, '4': 191154276, '5': 180915260,
+  '6': 171115067, '7': 159138663, '8': 146364022, '9': 141213431, '10': 135534747,
+  '11': 135006516, '12': 133851895, '13': 115169878, '14': 107349540, '15': 102531392,
+  '16': 90354753, '17': 81195210, '18': 78077248, '19': 59128983, '20': 63025520,
+  '21': 48129895, '22': 51304566, 'X': 155270560, 'Y': 59373566, 'M': 16571,
+}
+
+def ds_chrm_coor(cell_val=0, log=True):
+  '''
+    d[chrm] : list(each cell is a coordinate)
+    Cells values will be initialized to cell_val
+  '''
+  d_to_list = {}
+  for chrm, size in _human_genome_sizes.iteritems():
+    if log:
+      print >> sys.stderr, "ds_chrm_coor():" , chrm , size , "mem:" , memory_usage()
+    d_to_list[chrm] = [cell_val] * size
+
+  return d_to_list
 
 def setup_logging(logging):
   logging.basicConfig(format='%(asctime)s >> %(message)s ', level=logging.DEBUG)
@@ -92,3 +114,6 @@ def mkdir_p(path):
       pass
     else:
       raise
+
+def canonic_chrm(s):
+  return re.sub(r'(^[cC]hrm?)', '', s)
