7bf7704b205ad1c11ba8e5f72c60768d78a14c8a
David Rio Deiros <driodeiros@gmail.com>
2013-08-26 14:54:26 -0400

    Making the plotting of the staturation data much more flexible.

--- downstream/saturation_snps/plot_sat.py
+++ downstream/saturation_snps/plot_sat.py
@@ -12,48 +12,45 @@ import sys
 import matplotlib.pyplot as plt
 from drdmath import log_it
 
-def plot(ofn, x, y, title, xlabel, ylabel):
-  x_ticks = [i for i in range(1,int(max(x))+5) if i % 5 == 0 or i == 1]
+_usage = """
+Usage:
+  cat ../saturation/old/all/staturation.all.txt  | awk '{print $1" "$2}' | grep -v num |\
+  plot_stat.py "title" "x_label" "y_label" > output.png
+"""
+
+def plot(x, y, title, xlabel, ylabel):
+  #x_ticks = [i for i in range(1,int(max(x))+5) if i % 5 == 0 or i == 1]
   dot_size=12
 
   fig = plt.figure()
   ax = fig.add_subplot(1, 1, 1)
-  ax.set_xticks(ticks=x_ticks)
+  #ax.set_xticks(ticks=x_ticks)
   ax.scatter(x, y, dot_size)
   plt.xlabel(xlabel)
   plt.ylabel(ylabel)
   plt.grid(True)
   plt.title(title)
-  plt.savefig(ofn, dpi=400, bbox_inches='tight')
+  plt.savefig(sys.stdout, dpi=400, bbox_inches='tight')
 
 def process_data(fd):
-  h = {'n_samples': [], 'n_subs': [], 'n_indels': [] }
-  first_line = True
+  x, y = [], []
   for line in fd:
-    if not first_line and line != "\n":
-      n_samples, n_subs, n_indels  = line.rstrip().split('\t')
-      h['n_samples'].append(float(n_samples))
-      h['n_subs'].append(float(n_subs))
-      h['n_indels'].append(float(n_indels))
-    else:
-      first_line = False
-  return pd.DataFrame(h)
+    s = line.rstrip().split()
+    if len(s) == 2:
+      _id, count = s
+      x.append(_id)
+      y.append(count)
+  return x, y
 
 def main():
-  if len(sys.argv) == 1:
-    fd = drdcommon.xopen("-")
-    df = process_data(fd)
-    title="Saturation of substitutions CRV WGS"
-    plot("crv.subs.saturation.png", \
-      df['n_samples'], df['n_subs'], title, xlabel="# samples", ylabel="# uniq substitutions")
+  if not drdcommon.data_in_stdin():
+    drdcommon.error("I need a data stream in stdin.", usage=_usage)
+  if not len(sys.argv) == 4:
+    drdcommon.error("Wrong number of parameters", usage=_usage)
 
-    title="Saturation of indels CRV WGS"
-    plot("crv.indels.saturation.png", \
-      df['n_samples'], df['n_indels'], title, xlabel="# samples", ylabel="# uniq indels")
-
-    fd.close()
-  else:
-    drdcommon.error("Wrong number of args. Just need std values in stdin.")
+  title, _xl, _yl = sys.argv[1:]
+  x, y = process_data(drdcommon.xopen("-"))
+  plot(x, y, title, xlabel=_xl, ylabel=_yl)
 
 if __name__ == "__main__":
   main()
