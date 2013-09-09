8874b4e2f6ff7a8d509c97527221409069fd0331
David Rio Deiros <driodeiros@gmail.com>
2013-09-09 09:41:56 -0400

    Normalize fasta file.

---
+++ downstream/cnv/validation/normalize_ref.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+import sys, os.path
+import drdcommon
+
+max_c = int(sys.argv[1]) if len(sys.argv) == 2 else 80
+nc = -1
+for l in sys.stdin:
+  if l[0] == '>':
+    sys.stdout.write(l)
+    nc = 0
+  else:
+    for c in l.rstrip():
+      nc += 1
+      sys.stdout.write(c)
+      if nc == max_c:
+        print ""
+        nc = 0
+print ""
