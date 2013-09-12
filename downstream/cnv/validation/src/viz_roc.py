1c87d859bb0766b90cdfbde34a0e72c27d946056
David Rio Deiros <driodeiros@gmail.com>
2013-09-12 12:02:51 -0400

    Adding labels for threshold

--- downstream/cnv/validation/src/viz_roc.py
+++ downstream/cnv/validation/src/viz_roc.py
@@ -23,25 +23,26 @@ def load_data():
   for s in iter_lines('roc.data*.txt'):
     if len(s) == 4:
         min, th, sen, fdr = s # TODO: we are not using the min var
-        if th not in data:
-          data[th] = {}
-        data[th][sen] = fdr
+        data[th] = (sen, fdr)
   return data
 
 def do_work():
-  fig       = figure(figsize=(14,12))
-  ax        = fig.add_subplot(1, 1, 1)
-  for th, h2 in load_data().iteritems():
-      fdrs, sens = [], []
-      for s in sorted(h2.iterkeys()): # get all the (fdr, sens) for a particular th
-          sens.append(s)
-          fdrs.append(h2[s])
-      # have data(fdrs, sens) for a truth set; and particular threshold
-      ax.set_title("Performance CNV caller")
-      ax.set_ylabel("Sensitivity")
-      ax.set_xlabel("False Discovery Rate")
-      ax.plot(fdrs, sens,'o', markersize=10, label=th)
-      ax.legend(loc='best', shadow=True)
+  fig  = figure(figsize=(14,12))
+  ax   = fig.add_subplot(1, 1, 1)
+  thds = []
+  fdrs, sens = [], []
+  for th, metrics in load_data().iteritems():
+    thds.append(th)
+    fdrs.append(metrics[1])
+    sens.append(metrics[0])
+  print thds, fdrs, sens
+
+  ax.plot(fdrs, sens, 'o', markersize=10)
+  ax.set_title("Performance CNV caller")
+  ax.set_ylabel("Sensitivity")
+  ax.set_xlabel("False Discovery Rate")
+  for t, f, s in zip(thds, fdrs, sens):
+    ax.annotate(t, xy=(f, s))
   savefig('roc.png', dpi=200, bbox_inches='tight')
 
 matplotlib.rcParams.update({'font.size': 16, 'family': 'bold'})
