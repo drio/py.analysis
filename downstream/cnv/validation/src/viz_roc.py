65d3d86d61d0f7309709e927a1fbe6e08ce3c595
David Rio Deiros <driodeiros@gmail.com>
2013-09-12 17:12:48 -0400

    Minor touches in ROC viz.

--- downstream/cnv/validation/src/viz_roc.py
+++ downstream/cnv/validation/src/viz_roc.py
@@ -4,7 +4,7 @@
 #import numpy as np
 # from pandas import DataFrame, read_table
 from matplotlib.pyplot import *
-#from pylab import *
+from pylab import *
 from collections import defaultdict
 import drdcommon
 import argparse, sys, glob
@@ -22,28 +22,29 @@ def load_data():
   data = {}
   for s in iter_lines('roc.data*.txt'):
     if len(s) == 4:
-        min, th, sen, fdr = s # TODO: we are not using the min var
-        data[th] = (sen, fdr)
+      min, th, sen, fdr = s # TODO: we are not using the min var
+      data[float(fdr)] = float(sen)
   return data
 
 def do_work():
   fig  = figure(figsize=(14,12))
   ax   = fig.add_subplot(1, 1, 1)
-  thds = []
   fdrs, sens = [], []
-  for th, metrics in load_data().iteritems():
-    thds.append(th)
-    fdrs.append(metrics[1])
-    sens.append(metrics[0])
-  print thds, fdrs, sens
+  data = load_data()
+  for f in sorted(data.keys()):
+    fdrs.append(f)
+    sens.append(data[f])
+  print fdrs, sens
 
-  ax.plot(fdrs, sens, 'o', markersize=10)
+  ax.plot(fdrs, sens, 'o-', markersize=5)
   ax.set_title("Performance CNV caller")
   ax.set_ylabel("Sensitivity")
   ax.set_xlabel("False Discovery Rate")
-  for t, f, s in zip(thds, fdrs, sens):
-    ax.annotate(t, xy=(f, s))
-  savefig('roc.png', dpi=200, bbox_inches='tight')
+  xlim([0,1])
+  ylim([0,1])
+  #for t, f, s in zip(thds, fdrs, sens):
+  #  ax.annotate(t, xy=(f, s))
+  savefig('roc.png', dpi=250, bbox_inches='tight')
 
 matplotlib.rcParams.update({'font.size': 16, 'family': 'bold'})
 do_work()
