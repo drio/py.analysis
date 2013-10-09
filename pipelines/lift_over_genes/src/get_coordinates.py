d18f779d726adf05507201ea2e2fc6314d98269b
David Rio Deiros <driodeiros@gmail.com>
2013-10-09 15:21:50 -0400

    Enumerate exons instead of genes.

--- pipelines/lift_over_genes/src/get_coordinates.py
+++ pipelines/lift_over_genes/src/get_coordinates.py
@@ -22,19 +22,20 @@ for l in drdcommon.xopen(sys.argv[1]):
   gene_names[name] = True
 drdcommon.log("%s genes loaded." % len(gene_names))
 
-found = 0
 for l in drdcommon.xopen("-"):
   s = l.split("\t")
   if s[2] == "CDS":
     chrm, start, end, _list = s[0], s[3], s[4], s[8]
-    name = None
+    g_name, e_name, t_name = None, None, None
+
     for e in _list.split(";"):
       _ = e.split()
+      if len(_) == 2 and _[0] == "transcript_name":
+        t_name = re.sub('\"', '', _[1])
       if len(_) == 2 and _[0] == "gene_name":
-        name = re.sub('\"', '', _[1])
-        if name in gene_names:
-          print "chr%s %s %s %s" % (chrm, start, end, name)
-          found += 1
-          del gene_names[name]
-
-drdcommon.log("%s genes found." % found)
+        g_name = re.sub('\"', '', _[1])
+      if len(_) == 2 and _[0] == "exon_number":
+        e_num = re.sub('\"', '', _[1])
+      if g_name in gene_names and g_name and e_num and t_name:
+        print "chr%s %s %s %s %s %s" % (chrm, start, end, g_name, e_num, t_name)
+        break
