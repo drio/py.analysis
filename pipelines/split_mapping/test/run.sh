1ad2014b040db07d9e87863c903fed37b6823814
David Rio Deiros <driodeiros@gmail.com>
2013-11-19 12:37:13 -0500

    Adding bin dir. Check sapi is in path.

--- pipelines/split_mapping/test/run.sh
+++ pipelines/split_mapping/test/run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+[ $(which sapi.py) ] || (echo "sapi.py not found"; exit 1)
+
 curl -L http://cl.ly/4638453t2Q0E/phix_dataset.tar.bz2 |  tar -jx
 
-export PATH="$PATH:../"
+
 sapi.py -b $HOME/dev/bam.examples/phix.bam fastqc | bash
 sapi.py -b $HOME/dev/bam.examples/phix.bam  -n 10000 splits | bash
 sapi.py -b $HOME/dev/bam.examples/phix.bam  -f $HOME/dev/genomes/phix.fa sais | bash
