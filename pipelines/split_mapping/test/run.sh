1cc141098a4d111788af483dfe2bd3fb6cbc9e0b
David Rio Deiros <driodeiros@gmail.com>
2013-11-19 12:47:42 -0500

    use full path

--- pipelines/split_mapping/test/run.sh
+++ pipelines/split_mapping/test/run.sh
@@ -5,10 +5,12 @@
 curl -L http://cl.ly/4638453t2Q0E/phix_dataset.tar.bz2 |  tar -jx
 
 
-sapi.py -b $HOME/dev/bam.examples/phix.bam fastqc | bash
-sapi.py -b $HOME/dev/bam.examples/phix.bam  -n 10000 splits | bash
-sapi.py -b $HOME/dev/bam.examples/phix.bam  -f $HOME/dev/genomes/phix.fa sais | bash
-sapi.py -b $HOME/dev/bam.examples/phix.bam  -f $HOME/dev/genomes/phix.fa sampe | bash
+bam="`pwd`/phix/phix.bam"
+fa="`pwd`/phix/phix.fa"
+sapi.py -b $bam fastqc | bash
+sapi.py -b $bam  -n 10000 splits | bash
+sapi.py -b $bam -f $fa sais | bash
+sapi.py -b $bam  -f $fa sampe | bash
 sapi.py merge | bash
 sapi.py dups | bash
 sapi.py stats | bash
