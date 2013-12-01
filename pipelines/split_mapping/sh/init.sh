f7547cd5178218f7ac9616015ebf508bb2d5cd6d
David Rio Deiros <driodeiros@gmail.com>
2013-12-01 08:02:37 -0500

    A faster way to compute the number of reads.

--- pipelines/split_mapping/sh/init.sh
+++ pipelines/split_mapping/sh/init.sh
@@ -6,7 +6,10 @@ cd init
 BAM=$1
 NRS=$2
 
-total=`samtools view $BAM |wc -l`
+[ ! -f ${BAM}.bai ] && samtools index $BAM
+
+date > time.txt
+total=`samtools idxstats $BAM | awk '{s+=$3+$4} END {print s}'`
 cof=`echo "$total/$NRS" |bc`
 rem=`echo "$total%$NRS" |bc`
 nsplits=$cof
@@ -16,3 +19,4 @@ fi
 echo $cof > cof.txt
 echo $rem > rem.txt
 echo $nsplits > nsplits.txt
+date >> time.txt
