6fe2d67cbcbb6b2d0f7f0533ce1c1de1aa731393
David Rio Deiros <driodeiros@gmail.com>
2013-12-04 15:46:25 -0500

    Remove intermediate files in last step.

--- pipelines/split_mapping/sh/dups.sh
+++ pipelines/split_mapping/sh/dups.sh
@@ -13,10 +13,6 @@ java -Xmx${MEM} -jar $PICARD/MarkDuplicates.jar \
     METRICS_FILE=metrics.txt \
     OUTPUT=merged.sorted.dups.bam
 
-
-
-
-
-
-
-
+rm -f ../merge/*.bam
+rm -f ../sampe/*.bam
+rm -f ../splits/*.bam
