2efd6b56001faff4d011b6c218d0dacb9c5cbc3b
David Rio Deiros <driodeiros@gmail.com>
2013-12-09 10:10:57 -0500

    The rest service has changed. Modifying the cli client accordingly.

--- pipelines/split_mapping/signal.py
+++ pipelines/split_mapping/signal.py
@@ -5,10 +5,10 @@ import requests
 import json
 
 
-def update_resource(url=None, name=None, step=None, sos=None):
+def update_resource(url=None, name=None, step=None):
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
-    if url and name and step and sos in ["start", "stop"]:
+    if url and name and step:
         r = requests.get(url).json()
         _id = None
         for s in r["samples"]:  # Pick the first one !!
@@ -20,12 +20,12 @@ def update_resource(url=None, name=None, step=None, sos=None):
             print r
             _id = r.json()["sample"]["id"]
 
-        data = {'start_stop': sos, 'step': step}
+        data = {'step': step}
         r = requests.put(url + "/" + str(_id), data=json.dumps(data), headers=headers)
 
-if len(sys.argv) == 5:
-    url, name, step, sos = sys.argv[1:5]
-    update_resource(url, name, step, sos)
+if len(sys.argv) == 4:
+    url, name, step = sys.argv[1:4]
+    update_resource(url, name, step)
 else:
     if len(sys.argv) == 3 and sys.argv[1] == "list":
         url = sys.argv[2]
@@ -33,5 +33,5 @@ else:
         print json.dumps(data, sort_keys=True, indent=4)
     else:
         sys.stderr.write("Usage:\n")
-        sys.stderr.write("$ tool <resource_url> <sample_name> <step> <start or stop>\n")
+        sys.stderr.write("$ tool <resource_url> <sample_name> <step>\n")
         sys.stderr.write("$ tool list <resource_url>\n")
