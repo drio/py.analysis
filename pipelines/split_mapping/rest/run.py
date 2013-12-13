56c50b4c910e51e6507960d5dd6426cb1d866074
David Rio Deiros <driodeiros@gmail.com>
2013-12-13 11:41:07 -0500

    nc

--- pipelines/split_mapping/rest/run.py
+++ pipelines/split_mapping/rest/run.py
@@ -6,15 +6,15 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
+env_is = 'production'
+
 app.setup_redis()
 
-# Dev
-app.run(debug=True)
-# Production
-"""
-http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5000)
-IOLoop.instance().start()
-"""
+if env_is == 'dev':
+    app.run(debug=True)
+else:
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(5000)
+    IOLoop.instance().start()
 
 #url_for('static', filename='frontend.html')
