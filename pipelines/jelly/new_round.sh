4f8a455ff2e5de0d7f608f7a942b5ba807253ae1
David Rio Deiros <driodeiros@gmail.com>
2014-01-21 11:05:45 -0500

    Updating protocol.

--- pipelines/jelly/new_round.sh
+++ pipelines/jelly/new_round.sh
@@ -10,21 +10,17 @@ prev=round$curr
 dir=round$[$curr+1]
 base=`pwd`/$dir
 ref=`pwd`/$dir/reference/input.fasta
+
 cat << EOF
-mkdir -p $dir/reference
 mkdir -p $dir/logs
 
-cp -r ../boilerplate/* ./$dir/
-rm -f $dir/new_round.sh
-ln -s `pwd`/$prev/jelly.out.fasta ./$dir/reference/input.fasta
-rm -f $dir/reference/ml.fasta
 rm -rf ./$dir/reads
 ln -s `pwd`/$prev/reads ./$dir/
 
-cat ./$dir/Protocol.xml | \
-  sed 's:__REF__:$ref:g' | \
-  sed 's:__OUT__:$base:g' | \
-  sed 's:__BASE__:$base:g' > _tmp
-mv _tmp ./$dir/Protocol.xml
+mkdir -p $dir/reference
+ln -s `pwd`/$prev/jelly.out.fasta ./$dir/reference/input.fasta
 
+cp -r ../boilerplate/stats ./$dir/
+
+cat `pwd`/$prev/Protocol.xml | sed 's:$prev:$dir:g' > ./$dir/Protocol.xml
 EOF
