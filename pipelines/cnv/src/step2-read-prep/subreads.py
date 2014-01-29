2c6b3a587ab7be004b87011a8d5e710c169ffb80
David Rio Deiros <driodeiros@gmail.com>
2014-01-29 14:33:18 -0500

    Generate fastq instead of fasta

--- pipelines/cnv/src/step2-read-prep/subreads.py
+++ pipelines/cnv/src/step2-read-prep/subreads.py
@@ -10,8 +10,10 @@ import drdcommon
 i = 1
 for l in drdcommon.xopen("-"):
     seq = re.split(r'\t', l)[9]
-    _one, _two = seq[10:45+1], seq[46:81+1]
-    sys.stdout.write(">%s\n%s\n" % (i, _one))
-    sys.stdout.write(">%s\n%s\n" % (i+1, _two))
+    qua = re.split(r'\t', l)[10]
+    _one_s, _two_s = seq[10:45+1], seq[46:81+1]
+    _one_q, _two_q = qua[10:45+1], qua[46:81+1]
+    sys.stdout.write("@%s\n%s\n+\n%s\n" % (i, _one_s, _one_q))
+    sys.stdout.write(">%s\n%s\n+\n%s\n" % (i+1, _two_s, _two_q))
     i += 2
-sys.stderr.write("%s sub-reads generated, # of reads in input stream = %s\n" % (i, i/2))
+sys.stderr.write("%s sub-reads generated.\n" % i)
