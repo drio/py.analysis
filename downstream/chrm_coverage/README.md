90b3762e53d8a2a8e1c2fc76ce468a6c286f74ed
David Rio Deiros <driodeiros@gmail.com>
2014-03-10 11:39:28 +0100

    Generate per chrm coverage plots.

---
+++ downstream/chrm_coverage/README.md
@@ -0,0 +1,2 @@
+Generate coverage plots per chrm.
+
---
+++ downstream/chrm_coverage/all_plot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+
+for cov_f in *.gz
+do
+  for chrm in `cat genome.bed  | awk '{print $1}' | grep -v random`
+  do
+    sample=`echo $cov_f | sed 's/.bam.bed.gz//g'`
+    submit -s ${sample}_${chrm}_png -m8G "gzip -cd $cov_f | grep -P \"^${chrm}\t\" | ./plot.py ${sample}_${chrm} > ${sample}.${chrm}.png"
+  done
+done
+
