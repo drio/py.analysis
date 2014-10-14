1466e258b26d386dab0cabf499b1a203b1ae3a11
David Rio Deiros <driodeiros@gmail.com>
2014-10-14 10:11:57 -0400

    Step0

---
+++ pipelines/cnv/src/step0-rmdups/run-pipe.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+#
+# vim: set ts=4 sw=4 ft=python:
+import sys
+import os
+from itertools import chain
+
+def trimed_lines_no_comments(f):
+    return chain(l.rstrip() for l in open(f) if l[0] != "#")
+
+def get_fasta(common="../common.sh"):
+    for l in trimed_lines_no_comments(common):
+        var, val = l.split("=")
+        if var == "fasta":
+            return val[1:-1]
+    return None
+
+def submit(cmd, name):
+    print "echo '%s' | submit -s %s -m 8G -c 4" % (cmd, name)
+
+def bwa(config, fasta):
+    # RG fa fa fq1 fa fq2 fq1 fq2 out_name.bam
+    tmpl = 'bwa sampe -r "`cat %s`" %s <(bwa aln -t4 -1 %s %s) <(bwa aln -t4 -2 %s %s) %s %s |  samtools view -Shb /dev/stdin > %s.bam'
+    for idx, l in enumerate(open(config)):
+        fq1, fq2, lane, lib, sample = l.rstrip().split(' ')
+        _id = "%s_%s_%s" % (sample, lane, lib)
+        rg_f_name = "rg_%s" % _id
+        with open(rg_f_name, 'w') as rg_f:
+            rg = "@RG\tID:%s\tPL:ILLUMINA\tLB:%s\tSM:%s" % (_id, lib, sample)
+            rg_f.write(rg)
+            submit(tmpl % (rg_f_name, fasta, fasta, fq1, fasta, fq2, fq1, fq2, _id), "bwa" + _id)
+
+def merge(deps):
+    tmpl = 'java -Xmx8G -jar $PICARD/MergeSamFiles.jar \
+        TMP_DIR=/space1/tmp \
+        SORT_ORDER=coordinate \
+        USE_THREADING=true \
+        VALIDATION_STRINGENCY=LENIENT \
+        $INPUT \
+        OUTPUT=merged.sorted.bam'
+
+def dups():
+    tmpl = 'java -Xmx${MEM} -jar $PICARD/MarkDuplicates.jar \
+        TMP_DIR=$TMP_DIR \
+        METRICS_FILE=/dev/null \
+        VALIDATION_STRINGENCY=LENIENT \
+        INPUT=merged.sorted.bam \
+        OUTPUT=${ID}.merged.sorted.dups.bam'
+
+if __name__ == "__main__":
+    config = "config.txt"
+    if os.path.isfile(config):
+        bwa("config.txt", get_fasta())
+        #merge
+        #dups
+    else:
+        sys.stderr.write("config.txt not found, generating ...\n")
+        with open(config, 'w') as f:
+            f.write('fq_read1 fq_read2 lane_number library_id sample_id' + "\n")
+            f.write('reads/raw/French_1_1.fastq.gz reads/raw/French_1_2.fastq.gz 1 libXXXX French' + "\n")
+            f.write('reads/raw/French_2_1.fastq.gz reads/raw/French_2_2.fastq.gz 2 libBBBB French' + "\n")
+            f.write('...' + "\n")
